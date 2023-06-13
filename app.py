558c9579c63679557ad0a29c1a1ac66cfd88cbd3
Ajinkya Kale <127986789+ajinkode@users.noreply.github.com>
2023-06-13 17:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
-import streamlit as st;
+import mysql.connector
+import streamlit as st
+
+import sys
+st.header(sys.executable)
+
+# Establish a connection to MySQL Server
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="123ajinkya123",
+    database="pets"
+)
+
+mycursor=mydb.cursor()
+print("Connection Established")
+
+# Create Streamlit App
+
+def main():
+    st.title("CRUD Operations With MySQL");
+
+    # Display Options for CRUD Operations
+    option=st.sidebar.selectbox("Select an Operation",("Create","Read","Update","Delete"))
+    # Perform Selected CRUD Operations
+    if option=="Create":
+        st.subheader("Create a Record")
+        name=st.text_input("Enter Name")
+        email=st.text_input("Enter Email")
+        if st.button("Create"):
+            sql= "insert into users(name,email) values(%s,%s)"
+            val= (name,email)
+            mycursor.execute(sql,val)
+            mydb.commit()
+            st.success("Record Created Successfully!!!")
 
 
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+    elif option=="Read":
+        st.subheader("Read Records")
+        mycursor.execute("select * from users")
+        result = mycursor.fetchall()
+        for row in result:
+            st.write(row)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+
+
+    elif option=="Update":
+        st.subheader("Update a Record")
+        id=st.number_input("Enter ID",min_value=1)
+        name=st.text_input("Enter New Name")
+        email=st.text_input("Enter New Email")
+        if st.button("Update"):
+            sql="update users set name=%s, email=%s where id =%s"
+            val=(name,email,id)
+            mycursor.execute(sql,val)
+            mydb.commit()
+            st.success("Record Updated Successfully!!!")
+
+
+
+
+    elif option=="Delete":
+        st.subheader("Delete a Record")
+        id=st.number_input("Enter ID",min_value=1)
+        if st.button("Delete"):
+            sql="delete from users where id =%s"
+            val=(id,)
+            mycursor.execute(sql,val)
+            mydb.commit()
+            st.success("Record Deleted Successfully!!!")
+
+
+if __name__ == "__main__":
+    main()
